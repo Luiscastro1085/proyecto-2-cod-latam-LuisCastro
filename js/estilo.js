4ccc0c1a9d8ea3666b711a3311fa03bb6fb045ec
Luiscastro1085 <luiscastro1085@gmail.com>
2023-06-24 18:44:49 -0300

    cambios de JS

--- js/estilo.js
+++ js/estilo.js
@@ -3,3 +3,15 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('.header')
     header.classList.toggle('abajo', window.scrollY > 0)
 })
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
